8b85b8eceb1afe59b241e1936a5d0f76296e4da9
Thorsten Jakoby <tj@novatec-gmbh.de>
2017-02-03 11:05:50 +0100

    mavenize sub projects

--- MarioAI4J-Playground/compile.bat
+++
@@ -1,6 +0,0 @@
-cd bin
-rmdir /s /q ch
-cd ..
-dir /s /B *.java > sources.txt
-javac -d bin -cp bin;../MarioAI4J/bin;../MarioAI4J-Tournament/bin;../MarioAI4J-Tournament/lib/*;lib/* @sources.txt
-del /Q sources.txt
