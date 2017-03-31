b24cc64f77109c36581212154cac25a19c19fdb4
Dorian Weidler <dorian.weidler@hs-kl.de>
2017-03-31 11:06:46 +0200

    Levels for workshop 1 added.

--- MarioAI-Simulation/src/main/java/de/novatec/marioai/simulation/LevelConfig.java
+++ MarioAI-Simulation/src/main/java/de/novatec/marioai/simulation/LevelConfig.java
@@ -4,6 +4,31 @@ import ch.idsia.benchmark.mario.engine.generalization.Enemy;
 import ch.idsia.benchmark.mario.options.FastOpts;
 
 public enum LevelConfig {
+	
+	/**
+	 * Level for workshop 1 exercise 1
+	 */
+	HSKL_LEVEL_0(FastOpts.VIS_ON_2X + FastOpts.LEVEL_01_FLAT),
+	
+	/**
+	 * Level for workshop 1 exercise 2
+	 */
+	HSKL_LEVEL_1(FastOpts.VIS_ON_2X + FastOpts.LEVEL_01_FLAT + FastOpts.L_ENEMY(Enemy.GOOMBA, Enemy.GREEN_KOOPA)),
+	
+	/**
+	 * Level for workshop 1 exercise 3
+	 */
+	HSKL_LEVEL_2(FastOpts.VIS_ON_2X + FastOpts.LEVEL_01_FLAT + FastOpts.L_ENEMY(Enemy.GOOMBA, Enemy.GREEN_KOOPA, Enemy.SPIKY)),
+	
+	/**
+	 * Level for workshop 1 exercise 4
+	 */
+	HSKL_LEVEL_3(FastOpts.VIS_ON_2X + FastOpts.L_RANDOM_SEED(41) + FastOpts.L_GAPS_ON + FastOpts.L_PLATFORMS_ON + FastOpts.L_COINS_OFF),
+	
+	/**
+	 * Level for workshop 1 exercise 5
+	 */
+	HSKL_LEVEL_4(FastOpts.VIS_ON_2X + FastOpts.L_GAPS_ON + FastOpts.L_PLATFORMS_ON + FastOpts.L_TUBES_ON + FastOpts.L_ENEMY(Enemy.GOOMBA, Enemy.GREEN_KOOPA, Enemy.SPIKY)),
 
 	LEVEL_1(FastOpts.VIS_ON_2X + FastOpts.LEVEL_01_FLAT),
 
